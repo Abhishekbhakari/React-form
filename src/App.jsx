eeedd612584466149d47b6519dc3f07133bfe353
Abhishek Bhakari <130546310+Abhishekbhakari@users.noreply.github.com>
2024-01-20 15:22:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,52 @@
-// Main application component
 import React, { useState } from 'react';
 import FormComponent from './components/FormComponent/FormComponent';
 import TableComponent from './components/TableCoomponent/TableComponent';
 import EditModalComponent from './components/EditComponent/EditComponent';
 
 const App = () => {
-  // State for managing form data and table rows
+  // form data and table rows
   const [formData, setFormData] = useState([]);
-  // State for managing modal visibility and selected row data
+  // modal visibility and selected row data
   const [isModalOpen, setModalOpen] = useState(false);
   const [selectedRowData, setSelectedRowData] = useState(null);
 
-  // Function to handle form submission
+  //handle submission
   const handleFormSubmit = (data) => {
-    // Add new form data to the existing array
+    // Add new data to the existing array
     setFormData([...formData, data]);
   };
 
   // Function to handle edit button click
   const handleEdit = (index) => {
-    // Open modal and set selected row data for editing
     setModalOpen(true);
     setSelectedRowData(formData[index]);
   };
 
-  // Function to handle delete button click
+  // handle delete button 
   const handleDelete = (index) => {
-    // Create a copy of the form data array without the selected row
+    // Create a copy of data
     const updatedFormData = [...formData];
     updatedFormData.splice(index, 1);
-    // Update the form data state
+    // Update form 
     setFormData(updatedFormData);
   };
 
-  // Function to handle modal save
+  // modal save
   const handleModalSave = (updatedData) => {
-    // Find the index of the selected row in the form data array
+    // Find index 
     const index = formData.indexOf(selectedRowData);
-    // Create a copy of the form data array with updated row data
+    //  copy of the form updated data
     const updatedFormData = [...formData];
     updatedFormData[index] = updatedData;
-    // Update the form data state
+    // Update form 
     setFormData(updatedFormData);
     // Close the modal
     setModalOpen(false);
-    // Clear the selected row data
+    // Clear selected row data
     setSelectedRowData(null);
   };
 
-  // Function to handle modal close
+  //  modal close
   const handleModalClose = () => {
     // Close the modal
     setModalOpen(false);
@@ -58,14 +56,11 @@ const App = () => {
 
   return (
     <div>
-      {/* Form component */}
       <FormComponent onSubmit={handleFormSubmit} />
       <br />
 
-      {/* Table component */}
       <TableComponent data={formData} onEdit={handleEdit} onDelete={handleDelete} />
 
-      {/* Edit modal component */}
       {isModalOpen && (
         <EditModalComponent rowData={selectedRowData} onSave={handleModalSave} onClose={handleModalClose} />
       )}
